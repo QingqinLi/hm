f9f95d299ef60ec1724a3e4180f498b09896fcd8
QingqinLi <>
2019-06-13 21:05:37 +0800

    hm first commit

---
+++ hm_python_base/hm_01_data.py
@@ -0,0 +1,25 @@
+
+name = 'hm'
+# 文本对齐操作
+poem = ["xxxx",
+        "xxx",
+        "xxxxx",
+        "xxxxx",
+        "xxxxx",
+        "xxxxx",
+        ]
+for item in poem:
+    print(item.center(10, "*"))
+
+poem_str = "xxxx\t xxx\t xxxxx1\t \n xxxxx2\t\t xxxxx3\t\t"
+poem_list = poem_str.split()
+poem_new = " ".join(poem_list)
+print(poem_new)
+
+test_list = [1,2,4,5,6,7,8,5,4]
+for i in test_list:
+    print(i)
+    if i == 4:
+        continue
+else:
+    print("here")
