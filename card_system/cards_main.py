977c01d30b6b6e8c099aa3a4d7d95e2ecb070098
QingqinLi <>
2019-06-14 11:23:18 +0800

    完善初级名片管理系统的功能

--- card_system/cards_main.py
+++ card_system/cards_main.py
@@ -1,6 +1,10 @@
+#! /Library/Frameworks/Python.framework/Versions/3.6/bin/python3
+from card_system import cards_tools
 # 无限循环，由用户决定什么时候结束程序
+cards_tools.show_menu()
 while True:
     try:
+        # 可以不用int转换，直接判断用户输入的是不是字符串类型的数字，避免因为类型的原因导致程序报错
         select_operation = int(input("请选择要进行的操作："))
     except ValueError:
         print("请重新选择要进行的操作")
@@ -9,7 +13,12 @@ while True:
     if select_operation in [1, 2, 3]:
         # 正确的选择
         # pass关键字，是一个占位符，能够保证程序的代码结构正确，但没有任何的实际操作
-        pass
+        if select_operation == 1:
+            cards_tools.new_card()
+        elif select_operation == 2:
+            cards_tools.show_cards()
+        elif select_operation == 3:
+            cards_tools.search_card()
     elif select_operation == 0:
         # 退出系统
         print("欢迎再次使用【名片管理系统】")
---
+++ card_system/cards_tools.py
@@ -0,0 +1,110 @@
+# 定义一个全局变量
+cards_list = []
+
+
+def show_menu():
+    """
+    显示欢迎菜单
+    :return:
+    """
+    print("*" * 50)
+    print("【1】新增名片")
+    print("【1】展示名片")
+    print("【1】搜索名片")
+    print("*" * 50)
+
+
+def new_card():
+    """
+    定义一个新的名片
+    :return:
+    """
+    name = input("请输入姓名：")
+    age = input("请输入年龄:")
+    phone = input("请输入电话号码:")
+
+    card_dict = {'name': name,
+                 'age': age,
+                 'phone': phone,
+                 }
+    cards_list.append(card_dict)
+
+    print("新增名片成功")
+    print("=" * 50)
+
+
+def show_cards():
+    """
+    展示所有的名片
+    :return:
+    """
+    if len(cards_list) == 0:
+        print("当前无名片")
+        print("=" * 50)
+        return
+    print("姓名\t\t年龄\t\t电话")
+    for card in cards_list:
+        print(card['name'] + "\t\t" + card["age"] + "\t\t" + card["phone"])
+
+    print("已展示名片")
+    print("=" * 50)
+
+
+def search_card():
+    """
+    搜索卡片
+    :return:
+    """
+    card_name = input("请输入要搜索的名片姓名：")
+    for card in cards_list:
+        if card['name'] == card_name:
+            print("姓名\t\t年龄\t\t电话")
+            print(card['name'] + "\t\t" + card["age"] + "\t\t" + card["phone"])
+            card_info = card
+            select = input("请选择您要进行的操作："
+                           "【1】修改名片\t"
+                           "【2】删除名片:")
+            print(select)
+            if select == '1':
+                cards_list.append(update_card(card_info))
+                del_card(card_info)
+            elif select == '2':
+                del_card(card_info)
+            break
+    else:
+        print("未搜索到相关内容")
+    print("搜索名片完成")
+    print("=" * 50)
+
+
+def update_card(card_info):
+    """
+    修改卡片信息
+    :param card_info: 旧的card信息
+    :return:
+    """
+    name = input("请输入修改后的姓名【回车不修改】：")
+    age = input("请输入修改后的年龄【回车不修改】：")
+    phone = input("请输入修改后的电话【回车不修改】：")
+
+    if len(name) == 0:
+        name = card_info['name']
+    if len(age) == 0:
+        age = card_info['age']
+    if len(phone) == 0:
+        phone = card_info['phone']
+
+    card_new = {'name': name,
+                'age': age,
+                'phone': phone,
+                }
+    return card_new
+
+
+def del_card(card_info):
+    """
+    删除选择的卡片信息
+    :param card_info:
+    :return:
+    """
+    cards_list.remove(card_info)
