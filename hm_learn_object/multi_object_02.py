9d3f72fadcde45fc3242cd6279f33f5227f14189
QingqinLi <1589369708@qq.com>
2019-06-17 00:15:35 +0800

    'learn'

---
+++ hm_learn_object/multi_object_02.py
@@ -0,0 +1,19 @@
+class A:
+
+    def test(self):
+        print("test A")
+
+
+class B:
+
+    def demo(self):
+        print("demo 方法")
+
+
+class C(A, B):
+    pass
+
+
+c = C()
+c.test()
+c.demo()
