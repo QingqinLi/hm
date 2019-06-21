5f1191a442b95efb43cf82241557570b4687d915
QingqinLi <>
2019-06-21 20:47:14 +0800

    飞机大战

--- hm_plane_war/plane_sprites.py
+++ hm_plane_war/plane_sprites.py
@@ -3,10 +3,13 @@
 """
 __author__ = 'qing.li'
 """
+import random
 import pygame
-SCREEN_RECT = pygame.Rect(0, 0, 506, 846)
+SCREEN_RECT = pygame.Rect(0, 0, 501, 834)
 # 定义刷新帧率的常量
 FRAME_PER_SEC = 30
+ENEMY_EVENT = pygame.USEREVENT
+HREO_FIRE_EVENT = pygame.USEREVENT + 1
 
 
 class GameSprite(pygame.sprite.Sprite):
@@ -24,8 +27,6 @@ class GameSprite(pygame.sprite.Sprite):
     def update(self):
         # 重写update，在屏幕的垂直方向运动
         self.rect.y += self.speed
-        if self.rect.y >= 700:
-            self.rect.y = 0
 
 
 class BackGround(GameSprite):
@@ -43,4 +44,63 @@ class BackGround(GameSprite):
             self.rect.y = -self.rect.height
 
 
+class Enemy(GameSprite):
 
+    def __init__(self):
+        super().__init__("./images/plane_small.png")
+        self.speed = random.randint(1, 6)
+        self.rect.y = -self.rect.height
+        self.rect.x = random.randint(0, SCREEN_RECT.width-self.rect.width)
+
+    def update(self):
+        super().update()
+        if self.rect.y >= SCREEN_RECT.height:
+            self.kill()
+
+    def __del__(self):
+        # print("kill")
+        pass
+
+
+class Hero(GameSprite):
+
+    def __init__(self):
+        super().__init__("./images/plane.png", 0)
+        self.rect.centerx = SCREEN_RECT.centerx
+        self.rect.bottom = SCREEN_RECT.bottom - 120
+        # 创建子弹精灵组
+        self.bullets = pygame.sprite.Group()
+
+    # pygame捕获键盘按键：遍历事件列表，event判断类型 event.type == pygame.KEYDOWN；pygame的键盘模块，用户按下，对应的值为1，所有按键的元祖，用户可以按下不放
+    # pygame.key.get_pressed 返回所有按键元祖
+    def update(self):
+        # 获取用户动作
+        self.rect.x += self.speed
+        # 设置不可超出屏幕
+        if self.rect.x <= 0:
+            self.rect.x = 0
+        elif self.rect.x >= SCREEN_RECT.width - self.rect.width:
+            self.rect.x = SCREEN_RECT.width - self.rect.width
+
+    def fire(self):
+        for i in range(3):
+            # 创建子弹精灵
+            bullet = Bullet()
+            # 设置精灵的初始位置
+            bullet.rect.bottom = self.rect.y - 20 * i
+            bullet.rect.centerx = self.rect.centerx
+            # 将精灵添加到精灵组
+            self.bullets.add(bullet)
+
+
+class Bullet(GameSprite):
+    def __init__(self):
+        super().__init__("./images/bullet1.png", -2)
+
+    def update(self):
+        super().update()
+        if self.rect.bottom <= 0:
+            self.kill()
+
+    def __del__(self):
+        pass
--- hm_plane_war/play_main.py
+++ hm_plane_war/play_main.py
@@ -12,15 +12,20 @@ class PlaneMain(object):
         pygame.init()
         self.screen = pygame.display.set_mode((SCREEN_RECT.width, SCREEN_RECT.height))
         self.clock = pygame.time.Clock()
+        pygame.time.set_timer(ENEMY_EVENT, 1000)
+        pygame.time.set_timer(HREO_FIRE_EVENT, 500)
         self.__create_sprites()
-        print("yesy")
 
     def __create_sprites(self):
         # 创建北京精灵和精灵组
         bg = BackGround()
         bg2 = BackGround(True)
         sprite = GameSprite('./images/plane_small.png', 3)
-        self.sprite_group = pygame.sprite.Group(bg, bg2, sprite)
+        self.background_group = pygame.sprite.Group(bg, bg2, sprite)
+        self.enemy_group = pygame.sprite.Group()
+        self.hero = Hero()
+        self.hero_group = pygame.sprite.Group(self.hero)
+
         self.__update_sprites()
 
     def start_game(self):
@@ -36,19 +41,51 @@ class PlaneMain(object):
             # 更新显示
             pygame.display.update()
 
-    @staticmethod
-    def __event_handler():
+    def __event_handler(self):
         # 判断是否退出游戏
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 PlaneMain.__game_over()
+            elif event.type == ENEMY_EVENT:
+                enemy = Enemy()
+                self.enemy_group.add(enemy)
+            elif event.type == HREO_FIRE_EVENT:
+                self.hero.fire()
+        #     # 事件监听模式
+        #     elif event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+        #         print("move right")
+        # 事件键盘事件,判断元祖对应的索引,用户可以按下某个键不放
+        keys_pressed = pygame.key.get_pressed()
+        if keys_pressed[pygame.K_RIGHT]:
+            self.hero.speed = 3
+        elif keys_pressed[pygame.K_LEFT]:
+            self.hero.speed = -3
+        else:
+            self.hero.speed = 0
 
     def __check_collidate(self):
-        pass
+        # 子弹摧毁敌机
+        pygame.sprite.groupcollide(self.hero.bullets, self.enemy_group, True, True)
+        # 敌机摧毁英雄
+        enemies = pygame.sprite.spritecollide(self.hero, self.enemy_group, True)
+
+        if len(enemies) > 0:
+            self.hero.kill()
+            print("英雄牺牲，游戏结束")
+            PlaneMain.__game_over()
 
     def __update_sprites(self):
-        self.sprite_group.update()
-        self.sprite_group.draw(self.screen)
+        self.background_group.update()
+        self.background_group.draw(self.screen)
+
+        self.enemy_group.update()
+        self.enemy_group.draw(self.screen)
+
+        self.hero_group.update()
+        self.hero_group.draw(self.screen)
+
+        self.hero.bullets.update()
+        self.hero.bullets.draw(self.screen)
 
     @staticmethod
     def __game_over():
