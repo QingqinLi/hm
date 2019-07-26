e400b17222f499c4ba1ac773fd5f6b6de103ffba
QingqinLi <>
2019-07-26 22:01:01 +0800

    线程

---
+++ old_boy/网络基础/线程/信号量/信号量.py
@@ -0,0 +1,17 @@
+from threading import Semaphore, Thread
+import time
+
+
+def func(sem, i):
+    sem.acquire()
+    print('第%s个人进入屋子' % i)
+    time.sleep(2)
+    print('第%s个人离开屋子' % i)
+    sem.release()
+
+
+if __name__ == '__main__':
+    sem = Semaphore(10)
+    for i in range(20):
+        t = Thread(target=func, args=(sem, i))
+        t.start()
