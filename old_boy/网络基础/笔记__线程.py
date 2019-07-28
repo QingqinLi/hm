1e6b1eef60d250b32b0217cb31bdd393097b9c3a
QingqinLi <1589369708@qq.com>
2019-07-28 22:58:23 +0800

    '补充'

--- old_boy/网络基础/笔记__线程.py
+++ old_boy/网络基础/笔记__线程.py
@@ -13,9 +13,10 @@
 线程的使用方法：
     锁机制：
         递归锁：RLock() 可以有无止尽的锁，但是有一把万能钥匙
-        互斥锁：Lock() 一把钥匙一把锁
+        互斥锁：Lock() 一把钥匙一把锁，为了保护数据安全，  共享资源又叫临界资源，共享代码 又叫临界代码，对临界资源进行操作时，一定要加锁
         GIL: 全局解释器锁 所得是线程，CPython，意思是一个时间点只u 允许一个线程访问cpu
-    信号量：
+
+    信号量：也是用来保护数据安全
         from threading import Semaphore
     事件：
         from threading import Event
@@ -31,7 +32,9 @@
         from threading import Timer
         Timer(time, func) time:睡眠的事件（s), func，多久之后需要执行的任务
 
-
+同一个进程内的队列（多线程）：
+    import queue
+    queue.Queue() 先进先出
 
 
 """
